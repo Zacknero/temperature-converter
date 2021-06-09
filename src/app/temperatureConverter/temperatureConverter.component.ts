181cb629325b533c692755e9d2bbc5f2110f5985
Davide Zamballetti <davide.zamballetti@lynxspa.com>
2021-06-09 11:49:51 +0200

    problem solved

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'temperature-converter',
@@ -6,11 +6,18 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./temperatureConverter.component.scss']
 })
 
-export class TemperatureConverter implements OnInit {
-
-  ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
+export class TemperatureConverter {
+  
+  model = {
+    celsius: null,
+    fahrenheit: null
+  };
+  
+  convertToCelsius(fahrenheitInput: number) {
+    this.model.celsius = Math.round(((fahrenheitInput - 32) * 5 / 9) * 10) / 10;
   }
-
+  
+  convertToFahrenheit(celsiusInput: number) {
+    this.model.fahrenheit = Math.round(((celsiusInput * 9 / 5) + 32) * 10) / 10;
+  }
 }
